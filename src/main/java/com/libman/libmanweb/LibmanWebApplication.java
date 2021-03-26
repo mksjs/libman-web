e6787c48621e55c0c63d20c03ae4dd1256cbe3a9
Manish <mksjs1998@gmail.com>
2021-03-26 10:55:27 +0530

    Setup springBoot for libman-web

---
+++ src/main/java/com/libman/libmanweb/LibmanWebApplication.java
@@ -0,0 +1,13 @@
+package com.libman.libmanweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibmanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibmanWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libman/libmanweb/LibmanWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libman.libmanweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibmanWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
