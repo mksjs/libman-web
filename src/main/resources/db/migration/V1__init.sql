cc860ea5ac8ef959eb126bcde190eb8c6497da97
Manish <mksjs1998@gmail.com>
2021-03-27 10:57:02 +0530

    added flyway migration

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE book
+(
+	id bigint NOT NULL,
+ 	isbn varchar(100) NOT NULL,
+  	author varchar(50) NOT NULL ,
+ 	title varchar(100) DEFAULT NULL,
+ 	callnumber varchar(10) DEFAULT NULL,
+ 	publisher varchar(100) NOT NULL,
+  	year_of_publication varchar(20) NOT NULL ,
+ 	location varchar(100) DEFAULT NULL,
+ 	num_of_copies bigint NOT NULL,
+ 	current_status varchar(100) NOT NULL,
+  	keywords varchar(100) NOT NULL ,
+ 	last_available_date varchar(100) DEFAULT NULL,
+ 	wtUId bigint NOT NULL,
+ 	PRIMARY KEY (id)
+);
